0d23d6a321284dfec9929aa592af7e77a00d63dd
Di Yao <di.yao@vanderbilt.edu>
2018-01-24 13:31:23 -0600

    Added a runModifyRadioLinkSchedule() that modifies the properties of TxOp elements under each RadioLink in a database.

--- example_traverse_print.py
+++ example_traverse_print.py
@@ -20,7 +20,7 @@ import sys
 import pyorient
 
 class Processor(object):
-    def __init__(self, databaseName, configFile = 'config.json'):
+    def __init__(self, databaseName, textFile, configFile = 'config.json'):
         data_file= open(configFile, 'r')
         config_data = json.load(data_file)
         data_file.close()
@@ -37,6 +37,8 @@ class Processor(object):
             if 'username' in config_data['database'] and 'password' in config_data['database']:
                 self.db_username = config_data['database']['username']
                 self.db_password = config_data['database']['password']
+
+        self.textFile = textFile
 
     def openDatabase(self):
         """
@@ -122,10 +124,12 @@ class Processor(object):
         for key in record.oRecordData.keys():
             if 'Containment' not in key and 'Reference' not in key:
                 recordStr.append('{0}:{1} '.format(key, record.oRecordData[key]))
-        print "{0}                    {1}".format(record._class, str(recordStr))
+
+        print "{0}{1}{2}".format(record._class, createTabString(30-len(record._class)) , str(recordStr))
+        self.textFile.write("{0}{1}{2}\n".format(record._class, createTabString(30-len(record._class)) , str(recordStr)))
 
 
-    def runExample(self):
+    def runTraversePrint(self):
         """
         Retrieves the root vertex in the database.
         Calls the example traverse function on the root vertex.
@@ -143,10 +147,56 @@ class Processor(object):
             print "Unexpected error:", sys.exc_info()[0]
 
 
+    def runModifyRadioLinkSchedule(self):
+        """
+        Retrieves RadioLink vertices from the database.
+        Iterates through each RadioLink vertex and obtains its TxOp children.
+        The "StartUSec" and "StopUSec" properties of each TxOp child is incremented by 105.
+
+
+        :argument:
+        :return:
+        """
+
+        for v in self.client.command("select @rid, Name from V where @class ='RadioLink'"):
+            print '>>>>>>> {0} : {1}'.format(v.oRecordData['rid'], v.oRecordData['Name'])
+            self.textFile.write('\n\n>>>>>>> {0} : {1}\n'.format(v.oRecordData['rid'], v.oRecordData['Name']))
+            for v_2 in self.client.command("select from (traverse in('Containment') from {0}) where @class='TxOp'".format(v.oRecordData['rid'])):
+                print v_2
+                self.textFile.write(str(v_2.oRecordData))
+                self.textFile.write('\n')
+
+                self.client.command("update {0} set {1}={2}, {3}={4}".
+                                    format(v_2._rid, 'StopUSec', int(v_2.StopUSec) + 105, 'StartUSec', int(v_2.StartUSec) + 105))
+
+
+                #print "update {0} set {1}='{2}', {3}='{4}'".format(v_2._rid, 'StopUSec', int(v_2.StopUSec) + 100, 'StartUSec', int(v_2.StartUSec) + 100)
+
+            self.textFile.write('\n\n Post Modification\n')
+
+            for v_2 in self.client.command("select from (traverse in('Containment') from {0}) where @class='TxOp'".format(v.oRecordData['rid'])):
+                print v_2
+                self.textFile.write(str(v_2.oRecordData))
+                self.textFile.write('\n')
+
+
+""" Print formatting functions """
+def createTabString(numberTabs):
+    s = ''
+    i = 1
+    while i <= numberTabs:
+        s += ' '
+        i += 1
+    return s
+
+""""""
+
+
 def main(database, remotePlocal):
     """
     Instantiates a Processor object and passes in the orientDB database name.
-    Calls runExample() on the processor object.
+    Calls runTraversePrint().
+    Calls runModifyRadioLinkSchedule().
     Closes the orientDB database.
 
     :argument:
@@ -154,11 +204,20 @@ def main(database, remotePlocal):
                 remotePlocal (str): remote or local database, not used currently
     :return:
     """
-    processor=Processor(database)
+    textFile = open('example_traverse_print_LOG.txt', 'w')
+    processor=Processor(database, textFile)
 
     try:
         processor.openDatabase()
-        processor.runExample()
+
+        print '****************             Traverse Print              ****************'
+        textFile.write('****************             Traverse Print              ****************\n\n')
+        processor.runTraversePrint()
+
+        print '****************       Update RadioLink Schedule         ****************'
+        textFile.write('\n\n****************       Update RadioLink Schedule         ****************')
+        processor.runModifyRadioLinkSchedule()
+
         processor.closeDatabase()
     except:
         print "Unexpected error:", sys.exc_info()[0]
